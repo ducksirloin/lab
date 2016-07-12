f412e810307f1319699bac66b45ea0475c81b84a
duck_lab <neosw.my.no.1ability@gmail.com>
2016-07-12 12:32:33 +0900

    make groupfile py

--- makegroup.py
+++ makegroup.py
@@ -1,4 +1,5 @@
 fname = "groupfile.txt"
 f = open(fname,"w")
+f.write("#The group file for 360 pattern MD"+ "\n")
 for i in range(0,361):
-	f.write("sander -O -i ener.in -c confamb" + str(i) + ".inpcrd -p confamb" + str(i) + ".prmtop -o dih_ener" + str(i) + ".out -r dih_ener"+ str(i) + ".restrt -x dih_ener"+ str(i) + ".mdcrd" + "\n")
+	f.write("-O -i mdin.ener -c confamb" + str(i) + ".inpcrd -p confamb" + str(i) + ".prmtop -o dih_ener" + str(i) + ".out -r dih_ener"+ str(i) + ".restrt -x dih_ener"+ str(i) + ".mdcrd" + "\n")
