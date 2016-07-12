f953620f1ec18532a896010b637ba146bfe810c7
duck_lab <neosw.my.no.1ability@gmail.com>
2016-07-12 10:07:00 +0900

    new

--- makestr.py
+++ makestr.py
@@ -3,6 +3,6 @@ f = open(fname,"w")
 a = -180
 for i in range(0,361):
 	f.write("str" + str(i) + " = sequence{ACE GLH NME}" + "\n")
-	f.write("impose str" + str(i) + " {2} {{OE1 CD OE2 HE2 " + str(a+i) + "}}" + "/n")
-	f.write("saveAmberParm str" + str(i) + "  confamb" + str(i) + ".prmtop confamb" + str(i) + ".inpcrd")
+	f.write("impose str" + str(i) + " {2} {{OE1 CD OE2 HE2 " + str(a+i) + "}}" + "\n")
+	f.write("saveAmberParm str" + str(i) + "  confamb" + str(i) + ".prmtop confamb" + str(i) + ".inpcrd" + "\n")
 
