beb5e54f68e06e01a1221b66359e4f023c15bb1c
Tsuyoshi Yamagami <neosw.my.no.1ability@gmail.com>
2016-07-15 15:23:51 +0900

    edit result

--- result.py
+++ result.py
@@ -49,3 +49,59 @@ for f in open(fname,"r"):
         elif x[i] == "EELEC":
             f9.write(str(ang[8]) + " " + str(x[i+2]) + "\n")
             ang[8] += 1
+
+f1.close()
+f2.close()
+f3.close()
+f4.close()
+f5.close()
+f6.close()
+f7.close()
+f8.close()
+f9.close()
+
+Etot = []
+EPtot = []
+BOND = []
+ANGLE = []
+DIHED = []
+NB = []
+EEL = []
+VDWAALS = []
+EELEC = []
+
+for f in open(fname1,"r"):
+    x = f.split()
+    Etot.append(x[1])
+for f in open(fname2,"r"):
+    x = f.split()
+    EPtot.append(x[1])
+for f in open(fname3,"r"):
+    x = f.split()
+    BOND.append(x[1])
+for f in open(fname4,"r"):
+    x = f.split()
+    ANGLE.append(x[1])
+for f in open(fname5,"r"):
+    x = f.split()
+    DIHED.append(x[1])
+for f in open(fname6,"r"):
+    x = f.split()
+    NB.append(x[1])
+for f in open(fname7,"r"):
+    x = f.split()
+    EEL.append(x[1])
+for f in open(fname8,"r"):
+    x = f.split()
+    VDWAALS.append(x[1])
+for f in open(fname9,"r"):
+    x = f.split()
+    EELEC.append(x[1])
+
+a = -180
+fname10 = "all_result.txt"
+f = open(fname10,"w")
+f.write("dih  Etot    EPtot   BOND   ANGLE  DIHED   1-4NB  1-4EEL  VDWAALS  EELEC\n")
+for i in range(0,361):
+    f.write(str(a) + " " + str(Etot[i]) + " " + str(EPtot[i]) + " " + str(BOND[i]) + " " + str(ANGLE[i]) + " " + str(DIHED[i]) + " " + str(NB[i]) + " " + str(EEL[i]) + " " + str(VDWAALS[i]) + " " + str(EELEC[i]) + "\n")
+    a += 1
