6ae015d1b68ad2f6bcd2f3e66c6197b17f4ec788
Tsuyoshi Yamagami <neosw.my.no.1ability@gmail.com>
2016-07-15 15:24:18 +0900

    make files py

--- makefiles.py
+++ makefiles.py
@@ -5,13 +5,15 @@ for i in range(0,361):
 	f.write("set default PBradii mbondi2" + "\n")
 	f.write("str" + str(i) + " = sequence{ACE GLH NME}" + "\n")
 	f.write("impose str" + str(i) + " {2} {{OE1 CD OE2 HE2 " + str(a+i) + "}}" + "\n")
-	f.write("saveAmberParm str" + str(i) + "  confamb" + str(i) + ".prmtop confamb" + str(i) + ".inpcrd" + "\n")
+	f.write("saveAmberParm str" + str(i) + "  confamb.prmtop confamb" + str(i) + ".inpcrd" + "\n")
+f.close()
 
 fname2 = "groupfile.sh"
 f = open(fname2,"w")
 f.write("#!/bin/sh"+ "\n")
 for i in range(0,361):
-	f.write("sander -O -i ener.in -c confamb" + str(i) + ".inpcrd -p confamb" + str(i) + ".prmtop -o dih_ener" + str(i) + ".out -r dih_ener"+ str(i) + ".restrt -x dih_ener"+ str(i) + ".mdcrd" + "\n")
+	f.write("sander -O -i ener.in -c confamb" + str(i) + ".inpcrd -p confamb.prmtop -o dih_ener" + str(i) + ".out -r dih_ener"+ str(i) + ".restrt -x dih_ener"+ str(i) + ".mdcrd" + "\n")
+f.close()
 
 fname3 = "grep.sh"
 f = open(fname3,"w")
@@ -19,3 +21,4 @@ f.write("#!/bin/sh"+ "\n")
 f.write("grep -A4 -m1 NSTEP dih_ener0.out > allout.txt" + "\n")
 for i in range(1,361):
 	f.write("grep -A4 -m1 NSTEP dih_ener" + str(i) + ".out >> allout.txt" + "\n")
+f.close()
