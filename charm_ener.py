efd51ff349139b4e81037b24e7a851197ef7d7a5
Tsuyoshi Yamagami <neosw.my.no.1ability@gmail.com>
2016-07-29 11:04:02 +0900

    CHARMM energy out py

--- charm_ener.py
+++ charm_ener.py
@@ -1,16 +1,18 @@
-fname = "ener.out.xvg"
 fname2 = "charm_ener.txt"
-#name = []
-#for i in range(361):
-#    name.append("ener"+ str(i) + ".out.xvg")
-
+name = []
+for i in range(4):
+    name.append("ener"+ str(i) + ".out.xvg")
+a = -180
 f2 = open(fname2,"w")
-#for i in range(361):
-#    fname = name[i]
-f = open(fname,"r")
-x = f.readline()
-ener = x.split()
-for i in range(len(ener)):
-    f2.write(ener[i] + " ")
-f2.write("\n")
+f2.write("dihed_angle Proper-Dih Improper-Dih GB-Polarization Coulomb-14 Potential Kinetic-En Total-Energy\n")
+for i in range(4):
+    fname = str(name[i])
+    f = open(fname,"r")
+    x = f.readline()
+    ener = x.split()
+    f2.write(str(a) + " ")
+    a += 1
+    for i in range(1,len(ener)):
+        f2.write(ener[i] + " ")
+    f2.write("\n")
 f2.close()
