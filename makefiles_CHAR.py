8350625d0792cf2653a26824e4fd4262a0e83f4d
Tsuyoshi Yamagami <neosw.my.no.1ability@gmail.com>
2016-07-29 13:57:26 +0900

    make files for CHARMM py

--- makefiles_CHAR.py
+++ makefiles_CHAR.py
@@ -26,3 +26,23 @@ f = open(fname4, "w")
 for i in range(361):
 	f.write("~/tnagai/programs/gromacs-4.6.1-d-mpi-deb/bin/pdb2gmx_mpi_d -f GLH" + str(i) + ".pdb -o charmm" + str(i) + ".gro -p charmm" + str(i) + ".top -ignh -ter\n")
 f.close()
+
+fname5 = "do_sim_sh.csh"
+f = open(fname5,"w")
+for i in range(361):
+	f.write("~/tnagai/programs/gromacs-4.6.1-d-mpi-deb/bin/mdrun_mpi_d  -s  charmm" + str(i) + ".tpr  > logmd" + str(i) + ".log\n")
+f.close()
+
+fname6 = "do_ener_sh.csh"
+f = open(fname6,"w")
+for i in range(361):
+	f.write("~/tnagai/programs/gromacs-4.6.1-d/bin/g_energy_d -f  ener.edr  -s charmm" + str(i) + ".tpr -o ener.out -xvg none > log.ener <<EOF\n")
+	f.write("3 4 6 9 14 15 16 0\n")
+	f.write("EOF\n\n\nexit\n\n")
+f.write("End your selection with an empty line or a zero.\n-------------------------------------------------------------------\n")
+f.write("  1  Bond             2  U-B              3  Proper-Dih.      4  Improper-Dih. \n  5  CMAP-Dih.                            6  GB-Polarization                   \n")
+f.write("  7  Nonpolar-Sol.    8  LJ-14            9  Coulomb-14      10  LJ-(SR)       \n 11  LJ-(LR)         12  Coulomb-(SR)    13  Coulomb-(LR)    14  Potential     \n")
+f.write(" 15  Kinetic-En.     16  Total-Energy    17  Conserved-En.   18  Temperature   \n 19  Pressure        20  Constr.-rmsd    21  Vir-XX          22  Vir-XY        \n")
+f.write(" 23  Vir-XZ          24  Vir-YX          25  Vir-YY          26  Vir-YZ        \n 27  Vir-ZX          28  Vir-ZY          29  Vir-ZZ          30  Pres-XX       \n")
+f.write(" 31  Pres-XY         32  Pres-XZ         33  Pres-YX         34  Pres-YY       \n 35  Pres-YZ         36  Pres-ZX         37  Pres-ZY         38  Pres-ZZ       \n 39  #Surf*SurfTen   40  T-Protein \n")
+f.close()
