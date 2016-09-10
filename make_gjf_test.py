c71635b2d0f57e259a2c9c16d70bfaa70ebf9e8a
Tsuyoshi Yamagami <neosw.my.no.1ability@gmail.com>
2016-09-10 21:27:30 +0900

    make .gjf test py

---
+++ make_gjf_test.py
@@ -0,0 +1,21 @@
+a = -180
+fname1 = "FA_dih.gjf"
+f = open(fname1,"w")
+f.write("%mem=130MW\n")
+f.write("%Chk=C:\usr\n")
+for i in range(5):
+    fname = "HCOOH.inpcrd"
+    n = 0
+    f.write("#T HF/6-311G Test\n\nCOOH\n\n0 1\n")
+    for f1 in open(fname,"r"):
+        x = f1.split()
+        if n == 2:
+            f.write("C1     "+str(round(float(x[0]), 3))+" "+str(round(float(x[1]), 3))+" "+str(round(float(x[2]), 3))+"  \n")
+            f.write("O1     "+str(round(float(x[3]), 3))+" "+str(round(float(x[4]), 3))+" "+str(round(float(x[5]), 3))+"  \n")
+        elif n == 3:
+            f.write("O2     "+str(round(float(x[0]), 3))+" "+str(round(float(x[1]), 3))+" "+str(round(float(x[2]), 3))+"  \n")
+            f.write("H1     "+str(round(float(x[3]), 3))+" "+str(round(float(x[4]), 3))+" "+str(round(float(x[5]), 3))+"  \n")
+        elif n == 4:
+            f.write("H2     "+str(round(float(x[0]), 3))+" "+str(round(float(x[1]), 3))+" "+str(round(float(x[2]), 3))+"  \n\n")
+            f.write("--Link1--\n")
+        n += 1
