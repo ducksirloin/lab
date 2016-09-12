00aeb6006ce0a94c1cfd8f5b563111a19bd28df3
Tsuyoshi Yamagami <neosw.my.no.1ability@gmail.com>
2016-09-12 12:26:15 +0900

    make files for AcOH py

--- makefiles_Ac.py
+++ makefiles_Ac.py
@@ -6,23 +6,23 @@ for i in range(0,361):
 	f.write("str" + str(i) + " = loadmol2 ACE.mol2\n")
 	f.write("check str" + str(i) + "\n")
 	f.write("mods = loadAmberParams ACE.frcmod\n")
-	f.write("impose str" + str(i) + " {1} {{OD1 CG OD2 HD2 " + str(a+i) + "}}" + "\n")
-	f.write("saveAmberParm str" + str(i) + "  confamb_Ac.prmtop confamb_Ac" + str(i) + ".inpcrd" + "\n")
+	f.write("impose str" + str(i) + " {1} {{OD1 CG OD2 HD2 " + str(a+i) + "}}\n")
+	f.write("saveAmberParm str" + str(i) + "  confamb_Ac.prmtop confamb_Ac" + str(i) + ".inpcrd\n")
 f.close()
 
 fname2 = "groupfile_Ac.sh"
 f = open(fname2,"w")
 f.write("#!/bin/sh"+ "\n")
 for i in range(0,361):
-	f.write("sander -O -i ener.in -c confamb_Ac" + str(i) + ".inpcrd -p confamb_Ac.prmtop -o dih_ener_Ac" + str(i) + ".out -r dih_ener_Ac"+ str(i) + ".restrt -x dih_ener_Ac"+ str(i) + ".mdcrd" + "\n")
+	f.write("sander -O -i ener.in -c confamb_Ac" + str(i) + ".inpcrd -p confamb_Ac.prmtop -o dih_ener_Ac" + str(i) + ".out -r dih_ener_Ac"+ str(i) + ".restrt -x dih_ener_Ac"+ str(i) + ".mdcrd\n")
 f.close()
 
 fname3 = "grep_Ac.sh"
 f = open(fname3,"w")
 f.write("#!/bin/sh"+ "\n")
-f.write("grep -A4 -m1 NSTEP dih_ener_Ac0.out > allout_Ac.txt" + "\n")
+f.write("grep -A4 -m1 NSTEP dih_ener_Ac0.out > allout_Ac.txt\n")
 for i in range(1,361):
-	f.write("grep -A4 -m1 NSTEP dih_ener_Ac" + str(i) + ".out >> allout_Ac.txt" + "\n")
+	f.write("grep -A4 -m1 NSTEP dih_ener_Ac" + str(i) + ".out >> allout_Ac.txt\n")
 f.close()
 
 fname4 = "grep_Ac_Gau.sh"
