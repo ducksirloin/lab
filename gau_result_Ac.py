7f5f2937926998e2a6e691d7cef2e922a2a302fc
Tsuyoshi Yamagami <neosw.my.no.1ability@gmail.com>
2016-09-12 12:25:32 +0900

    result of gaussian of AcOH py

--- gau_result_Ac.py
+++ gau_result_Ac.py
@@ -7,18 +7,17 @@ SCF = []
 for f in open(fname1, "r"):
     x = f.split()
     nuc.append(x[3])
-f.close()
 
 for f in open(fname2, "r"):
     x = f.split()
     SCF.append(x[4])
-f.close()
 
 a = -180
 f = open(fname3, "w")
-f.write("nuclear     SCF     sum     kcal/mol\n")
+f.write("dihed     nuclear     SCF     sum     kcal/mol\n")
 for i in range(361):
     s = float(nuc[i]) + float(SCF[i])
     kcal = s *  627.5095
     f.write(str(a) + " " + str(nuc[i]) + " " + str(SCF[i]) + " " + str(s) + " " + str(kcal) + "\n")
     a += 1
+f.close()
