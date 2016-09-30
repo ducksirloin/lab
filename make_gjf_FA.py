d086b1e76b007dd9cb383a3d0d69676994fbe96d
Tsuyoshi Yamagami <neosw.my.no.1ability@gmail.com>
2016-09-30 15:11:07 +0900

    make .gjf for FAOH py

--- make_gjf_FA.py
+++ make_gjf_FA.py
@@ -3,21 +3,21 @@ fname1 = "FA_dih.gjf"
 f = open(fname1,"w")
 f.write("%mem=130MW\n")
 f.write("%Chk=C:\Documents and Settings\user\デスクトップ\n")
-for i in range(361):
-    fname = "confamb_FA" + str(i) + ".inpcrd"
+for i in range(37):
+    fname = "confamb_FA" + str(i * 10) + ".inpcrd"
     n = 0
     f.write("#T HF/6-311G Test\n\nCOOH\n\n0 1\n")
     for f1 in open(fname,"r"):
         x = f1.split()
         if n == 2:
-            f.write("C1     "+str(x[0])+" "+str(x[1])+" "+str(2)+"  \n")
-            f.write("O1     "+str(x[3])+" "+str(x[4])+" "+str(5)+"  \n")
+            f.write("C1     "+str(x[0])+" "+str(x[1])+" "+str(x[2])+"  \n")
+            f.write("O1     "+str(x[3])+" "+str(x[4])+" "+str(x[5])+"  \n")
         elif n == 3:
-            f.write("O2     "+str(x[0])+" "+str(x[1])+" "+str(2)+"  \n")
-            f.write("H1     "+str(x[3])+" "+str(x[4])+" "+str(5)+"  \n")
+            f.write("O2     "+str(x[0])+" "+str(x[1])+" "+str(x[2])+"  \n")
+            f.write("H1     "+str(x[3])+" "+str(x[4])+" "+str(x[5])+"  \n")
         elif n == 4:
-            f.write("H2     "+str(x[0])+" "+str(x[1])+" "+str(2)+"  \n\n")
-        if n == 4 and i < 360:
+            f.write("H2     "+str(x[0])+" "+str(x[1])+" "+str(x[2])+"  \n\n")
+        if n == 4 and i < 36:
             f.write("--Link1--\n")
         n += 1
 f.close()
